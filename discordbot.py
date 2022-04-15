80ec43721b47d90ed071f3f6074287cc5b46706a
CK <73201351+shdw9@users.noreply.github.com>
2022-04-15 01:19:07 -0700

    2.0
    finds games, adds a reaction to when its finished

--- discordbot.py
+++ discordbot.py
@@ -1,122 +1,179 @@
-from sys import argv
-import discord
+from re import L
 from discord.ext import commands
-import cassiopeia as cass
-import datetime
+import discord, cassiopeia as cass, datetime, asyncio, requests, json, roleidentification
+
+# you need a riot API key (the development one works, but it'll expire after a few days)
+# I would suggest applying for one and getting approved for one
+riotAPI = ""
+
+# Edit this list with summoners you want to monitor
+watchedSummoners = ['BLACKCAR','lrradical','IlIlIIllIllIllIl','Randomdude2468','ASOKO TENSEI','Amumu Main','Yasuo Meister','KRÂM','Motoaki Tanigo','Drch1cken','Burnt Toast741']
+
+# The channel ID to send the notifications to
+sendNotificationsChannelID = ""
+
+#######################################################
+
+latestVersion = requests.get("https://ddragon.leagueoflegends.com/realms/na.json").json()["v"]
+championDatabase = requests.get("https://ddragon.leagueoflegends.com/cdn/" + latestVersion + "/data/en_US/championFull.json").json()
 
 bot = commands.Bot(command_prefix='!')
-cass.set_riot_api_key("RIOT_API")
+cass.set_riot_api_key(riotAPI)
+
+foundGames = []
+watchedGames = []
+
+def getSummoner(summoner):
+    return requests.get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summoner + "?api_key=" + riotAPI).json()
+
+def isPlaying(summonerID):
+    results = requests.get("https://na1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/" + summonerID + "?api_key=" + riotAPI)
+    if results.status_code != 200:
+        return False
+    else:
+        print("Match found!")
+        return True
 
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="LEAGUE OF LEGENDS"))
-    print("Ready!")
+    print('=> Logged in as {0.user}'.format(bot))
 
+async def background_task():
+    await bot.wait_until_ready()
+
+    while(True):
+        for x in range(len(watchedSummoners)):
+            await gamerCheck(watchedSummoners[x])
+            await asyncio.sleep(30)
+        await asyncio.sleep(120)
+        await gameCheck()
+ 
+# This command is lets you copy and paste the lobby joined messages 
+# and parses it into a na.op.gg link. All you have to do is copy and paste the link
 @bot.command()
-async def build(ctx, args):
+async def multisearch(ctx,*,args):
+    searchQuery = "https://na.op.gg/multisearch/na?summoners="
+    if "joined the lobby" in args: # ENGLISH CLIENT
+        for line in args.split("\n"):
+            name = line.split(" joined")[0].replace(" ","+")
+            searchQuery += name + "%2C"
+        print(searchQuery)
+        await ctx.reply(searchQuery[:len(searchQuery)-3])
+        try:
+            await ctx.message.delete()
+        except:
+            pass
+    elif "님이 로비에 참가하셨습니다" in args: # KOREAN CLIENT
+        for line in args.split("\n"):
+            name = line.split(" 님이")[0].replace(" ","+")
+            searchQuery += name + "%2C"
+        print(searchQuery)
+        await ctx.reply(searchQuery[:len(searchQuery)-3])
+        try:
+            await ctx.message.delete()
+        except:
+            pass
+    else:
+        await ctx.reply("Sorry I can't comprehend what you're saying! :sweat_smile: ")
+
+# This command sends you the information of a champion's passive and abilities descriptions and cooldowns
+@bot.command()
+async def abilities(ctx, *, args):
+    keys = { 0:"Q", 1:"W", 2:"E",3:"R" }
     embed=discord.Embed(timestamp=datetime.datetime.utcnow(), color=0x62C979)
-    if (args[0].islower()):
-        args = args.capitalize()
+    args = args.title().replace(" ","")
+    if args not in championDatabase["data"]:
+        await ctx.reply(content="That is not a valid champion! :x:\n\nYour input: `" + args + "`")
+        return
     try:
-        champicon = cass.Champion(name=args,region="NA").image().url
+        champicon = "https://ddragon.leagueoflegends.com/cdn/12.7.1/img/champion/" + args + ".png"
         embed.set_thumbnail(url=champicon)
     except:
-        await ctx.reply(content="That is not a valid champion! :x:")
-        
+        await ctx.reply(content="Could not find any data for " + args + " :x:")
         return
 
-    
-    embed.set_author(name=args + " - " + cass.Champion(name=args,region="NA").title, icon_url=champicon)
+    embed.set_author(name=args + " Information", icon_url=champicon)
     embed.set_footer(text="powered by shdw 👻")
-
     embed.add_field(name="NA.OP.GG",value="https://na.op.gg/champion/" + args + "/statistics/",inline=True)
+    embed.add_field(name="Passive - " + championDatabase["data"][args.title()]["passive"]["name"],value=championDatabase["data"][args.title()]["passive"]["description"],inline=False)
     try:
-        ally = cass.Champion(name=args,region="NA").ally_tips
-        enemy = cass.Champion(name=args,region="NA").enemy_tips
-        allytips = ""
-        enemytips = ""
-        for x in range(len(ally)):
-            allytips += "\n🔹 " + ally[x]
-        for x in range(len(enemy)):
-            enemytips += "\n🔸 " + enemy[x]
-        if len(ally) == 0:
-            allytips = "No ally tips can be found at the moment!"
-        if len(enemy) == 0:
-            enemytips = "No enemy tips can be found at the moment!"
-        embed.add_field(name="Ally Tips ⬇️",value=allytips,inline=False)
-        embed.add_field(name="Enemy Tips ⬇️",value=enemytips,inline=False)
-    except:
+        for x in range(len(championDatabase["data"][args.title()]["spells"])):
+            embed.add_field(name=keys[x] + " - " + championDatabase["data"][args.title()]["spells"][x]["name"],value=championDatabase["data"][args.title()]["spells"][x]["description"] + "\nCooldown: **" + str(championDatabase["data"][args.title()]["spells"][x]["cooldown"]) + "**" ,inline=False)
+    except Exception as e:
         pass
-    
     await ctx.send(embed=embed)
 
-@bot.command()
-async def profile(ctx, args):
-    try:
-        summoner = cass.get_summoner(name=args, region="NA")
-        print("\nSummoner Name: " + summoner.name)
-        print("Icon: " + summoner.profile_icon().url)
-        print("Level: " + str(summoner.level))
-        embed=discord.Embed(title=summoner.name + "'s profile",timestamp=datetime.datetime.utcnow(), color=0x62C979)
-        embed.description=summoner.champion_masteries[0].champion.__getattribute__('name') + " Main [" + str(summoner.champion_masteries[0].level) + "] with " + str(summoner.champion_masteries[0].points) + " points"
-        embed.set_author(name=summoner.name, icon_url=summoner.profile_icon().url)
-        embed.set_footer(text="powered by shdw 👻")
+# Iterates through watchedGames list, if match finished, check match results 
+# and then remove from watchedGames
+async def gameCheck():
+    global watchedGames
 
-        embed.add_field(name="Level",value=str(summoner.level), inline=True)
-        embed.add_field(name="NA.OP.GG",value="[Link](https://na.op.gg/summoner/userName=" + args +")",inline=True)
+    for game in watchedGames:
+        matchID = game[0]
+        summonerPUUID = game[1]
 
-        embed.set_thumbnail(url=summoner.profile_icon().url)
-        
-        await ctx.send(embed=embed)
-    except:
-        await ctx.reply(content="That profile does not exist! :x:")
+        matchResults = requests.get("https://americas.api.riotgames.com/lol/match/v5/matches/NA1_" + matchID + "?api_key=" + riotAPI)
+        if (matchResults.status_code == 200):
+            print("Match ID " + matchID + " has been finished")
+            matchData = matchResults.json()
+            key = matchData["metadata"]["participants"].index(summonerPUUID)
+            gameWon = matchData["info"]["participants"][key]["win"]
+            embed = game[2]
+            if (gameWon):
+                await embed.add_reaction("🇼")
+            else:
+                await embed.add_reaction("🇱")
+            watchedGames.remove(game)
 
-@bot.command()
-async def game(ctx, arg):
-    try:
-        summoner = cass.get_summoner(name=arg, region="NA")
-        print("\nSummoner Name: " + summoner.name)
-    except:
-        print("That summoner name does not exist!")
-        await ctx.reply(content="That summoner name does not exist! :x:")
-        return
-    try:
-        current = cass.get_current_match(summoner=arg,region="NA")
+
+champion_roles = roleidentification.pull_data()
+async def gamerCheck(name):
+    global foundGames
+    global watchedGames
+
+    summoner = getSummoner(name)
+    name = summoner["name"]
+    if (isPlaying(summoner["id"])):
+        current = cass.get_current_match(summoner=name,region="NA")
+        if current.id in foundGames:
+            return # game is already found, prevents spam
+
+        # format discord embed with teams, players, and champions
         blueteam = []
-        redteam = []
-        blueteamcomp = ""
-        redteamcomp = ""
-        currentChamp = ""
+        blueSummoners = {}
         for x in current.blue_team.participants:
-            blueteam.append(x.summoner.name + " - " + x.champion.name)
-            blueteamcomp += "\n" + x.summoner.name + " - " + x.champion.name
-            if (x.summoner.name == summoner.name):
-                currentChamp = x.champion.name
-                print("Current Champ: " + currentChamp)
+            blueSummoners[x.champion.id] = x.summoner.name
+            blueteam.append(x.champion.id)
+        blueteamroles = roleidentification.get_roles(champion_roles,blueteam)
+        blueteamcomp = ""
+        for x in blueteamroles:
+            blueteamcomp += "\n" + championDatabase["keys"][str(blueteamroles[x])] + " - `" + blueSummoners[blueteamroles[x]] + "`"
+
+        redteam = []
+        redSummoners = {}
         for x in current.red_team.participants:
-            redteam.append(x.summoner.name + " - " + x.champion.name)
-            redteamcomp += "\n" + x.summoner.name + " - " + x.champion.name
-        embed=discord.Embed(title=summoner.name + "'s Current Game",description="NA.OP.GG: [Link 🔗](https://na.op.gg/summoner/userName=" + arg +")",timestamp=datetime.datetime.utcnow(), color=0x62C979)
-        if " " in currentChamp:
-            linkName = currentChamp.replace(" ", "")
-        else:
-            linkName = currentChamp
-        gamemode = str(current.queue).split('Queue.')[1].capitalize()
-        if "_" in gamemode:
-            rawr = gamemode.replace("_"," ").title()
-            gamemode = rawr
-        embed.set_author(name=summoner.name + " is playing as " + currentChamp, icon_url=cass.Champion(name=currentChamp,region="NA").image().url, url="https://na.op.gg/champion/" + linkName + "/statistics/")
-        embed.set_thumbnail(url=summoner.profile_icon().url)
-        embed.set_footer(text="powered by shdw 👻")
-        print(current.queue)
-        embed.add_field(name="Gamemode",value=gamemode,inline = False)
+            redSummoners[x.champion.id] = x.summoner.name
+            redteam.append(x.champion.id)
+        redteamroles = roleidentification.get_roles(champion_roles,redteam)
+        redteamcomp = ""
+        for x in redteamroles:
+            redteamcomp += "\n" + championDatabase["keys"][str(redteamroles[x])] + " - `" + redSummoners[redteamroles[x]] + "`"
+
+        embed=discord.Embed(title=name + " Game Found!",description="NA.OP.GG: [Link 🔗](https://na.op.gg/summoner/userName=" + name.replace(" ","") +")",timestamp=datetime.datetime.utcnow(), color=0x62C979)
+        embed.set_thumbnail(url="https://ddragon.leagueoflegends.com/cdn/" + latestVersion + "/img/profileicon/" + str(summoner["profileIconId"]) + ".png")
+        embed.set_footer(text="Match ID: " + str(current.id) + " • powered by shdw 👻")
+        embed.add_field(name="Gamemode",value=str(current.queue).split('Queue.')[1].capitalize().replace("_"," ").title(),inline = False)
         embed.add_field(name="Blue Team",value=blueteamcomp, inline=True)
         embed.add_field(name="Red Team",value=redteamcomp, inline=True)
-        await ctx.send(embed=embed)
-        del blueteam
-        del redteam
-    except Exception as e:
-        print(e)
-        await ctx.reply(content=summoner.name + " is not currently in game! :x:")
+        sentEmbed = await bot.get_channel(int(sendNotificationsChannelID)).send(embed=embed)
 
-bot.run('BOTTOKEN')
+        # if successfully sent, add to foundGames, and watchedGames
+        # will add the following item:
+        # [matchID, summonerPUUID, discordEmbed]
+        foundGames.append(current.id)
+        watchedGames.append([str(current.id),summoner["puuid"],sentEmbed])
+    
+bot.loop.create_task(background_task())
+
+bot.run(')
