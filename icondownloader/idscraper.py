0e3ca6feeab1c9cf0cd23f60ad38d28f35aee9a1
CK <73201351+shdw9@users.noreply.github.com>
2022-11-29 01:24:49 -0800

    Update idscraper.py

--- icondownloader/idscraper.py
+++ icondownloader/idscraper.py
@@ -1,16 +1,24 @@
 from discord.ext import commands
-import discord, asyncio, datetime, random, asyncio, os
-from discord.ext.commands import CommandNotFound
+import requests
 
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
-    print('=> Logged in as {0.user}'.format(bot))
+    print("Pulling all emoji ids ...")
 
+    text = ""
     for guild in bot.guilds:
         for emoji in guild.emojis:
-            print(emoji.name, emoji.id) 
+            text += emoji.name + " " + str(emoji.id) + "\n"
+
+    print("Finished pulling all emojis!")
+
+    a = requests.post("https://hastebin.com/documents", data = text)
+    link = "https://hastebin.com/raw/" + a.json()["key"]
+
+    print("\nUse this link for emojiIds for Blitzcrank BOT:")
+    print(link)
     
 
-bot.run("token")
+bot.run("")
